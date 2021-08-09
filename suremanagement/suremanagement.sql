609cf36e5df07c69bbd92999abec715012264a37
Jeison Prieto <japs6903@gmail.com>
2021-08-09 07:10:50 -0500

    Update suremanagement.sql

--- suremanagement/suremanagement.sql
+++ suremanagement/suremanagement.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `suremanagement`
 --
-
+CREATE DATABASE suremanagement;
+USE suremanagement;
 -- --------------------------------------------------------
 
 --
